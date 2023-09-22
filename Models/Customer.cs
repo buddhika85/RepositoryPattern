6ab8a6fe24796ac71a44eb17868e466d6bcda69b
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-09-22 19:29:44 +0530

    models class lib added

---
+++ Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string CustomerGuid { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        public bool IsPremium { get; set; }
+    }
+}
