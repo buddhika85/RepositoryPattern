a92b1c1e0416f5740646358c3476e0723f102f70
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-09-25 03:06:52 +0530

    SQL repositories added

---
+++ CustomerAPI_RepositoryPattern/Data/ICustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using Models;
+
+namespace CustomerAPI_RepositoryPattern.Data
+{
+    public interface ICustomerRepository
+    {
+        Task SaveChangesAsync();
+        Task<Customer?> GetCustomerByIdAsync(string customerId);
+        Task<IEnumerable<Customer>> GetAllCustomersAsync();
+        Task CreateCustomerAsync(Customer customer);
+        // update
+
+        void DeleteCommand(Customer customer);
+    }
+}
---
+++ CustomerAPI_RepositoryPattern/Data/SqlCustomerRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace CustomerAPI_RepositoryPattern.Data
+{
+    public class SqlCustomerRepository : ICustomerRepository
+    {
+        private readonly AppDbContext _context;
+
+        public SqlCustomerRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Customer?> GetCustomerByIdAsync(string customerId)
+        {
+            return await _context.Customers.FirstOrDefaultAsync(x => x.CustomerGuid == customerId);
+        }
+
+        public async Task<IEnumerable<Customer>> GetAllCustomersAsync()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        public async Task CreateCustomerAsync(Customer customer)
+        {
+            if (customer is null)
+                throw new ArgumentNullException(nameof(customer));
+            await _context.Customers.AddAsync(customer);
+        }
+
+        public void DeleteCommand(Customer customer)
+        {
+            if (customer is null)
+                throw new ArgumentNullException(nameof(customer));
+            _context.Customers.Remove(customer);
+        }
+    }
+}
